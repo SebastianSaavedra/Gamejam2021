0ff958eac3bb18387d2e08d9464cf2cbb304a185
SebastianSaavedra <sebastiansaavedr@gmail.com>
2021-01-30 16:22:29 -0300

    Canvas de mundo & overlay, codigos de agua pt1

---
+++ Assets/02_Scripts/Fade.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    void Start()
+    {
+        Image imgFade = GetComponent<Image>();
+        imgFade.DOFade(0,5f);
+        Destroy(gameObject,5.1f);
+    }
+}
--- Assets/02_Scripts/WaterEvents.cs
+++ Assets/02_Scripts/WaterEvents.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class WaterEvents : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] LayerMask layer;
+    [SerializeField] List<GameObject> objetos = new List<GameObject>();
 
-    // Update is called once per frame
+    int clicks;
+    bool puedeDarClick = true;
+    bool primerEvento = false;
     void Update()
     {
-        
+        IsClicked();
+    }
+
+    void IsClicked()
+    {
+        if (puedeDarClick && Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse is pressed down " + puedeDarClick);
+
+            RaycastHit hitInfo = new RaycastHit();
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo,Mathf.Infinity,layer))
+            {
+                Debug.Log("Object Hit is " + hitInfo.collider.gameObject.name);
+
+                if (!primerEvento && hitInfo.collider.gameObject.name == "Agua")
+                {
+                    if (clicks != 5)
+                    {
+                        clicks++;
+                        objetos[clicks].SetActive(true);
+                    }
+                    else
+                    {
+                        Debug.Log("Clicks = 5");
+                        clicks = 0;
+                        StartCoroutine(Espera());
+                    }
+                }
+            }
+        }
+    }
+
+    IEnumerator Espera()
+    {
+        puedeDarClick = false;
+        primerEvento = true;
+        yield return new WaitForSeconds(3.5f);
+        Destroy(this);
+        //puedeDarClick = true;
+        yield break;
     }
 }
