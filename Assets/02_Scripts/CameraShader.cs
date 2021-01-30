c2be9291e40b5ff59a5a03a0e9d70eb946afff3a
SebastianSaavedra <sebastiansaavedr@gmail.com>
2021-01-30 09:37:14 -0300

    Primer commit o_o

---
+++ Assets/02_Scripts/CameraShader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraShader : MonoBehaviour
+{
+    private Shader outlineShader;
+    private List<SnapshotFilter> filters = new List<SnapshotFilter>();
+
+    private void Awake()
+    {
+        outlineShader = Shader.Find("Snapshot/EdgeDetect");
+        filters.Add(new BaseFilter("Outlines", Color.white, outlineShader));
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        filters[0].OnRenderImage(src, dst);
+    }
+}
