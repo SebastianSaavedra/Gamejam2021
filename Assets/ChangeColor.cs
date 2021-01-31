1b2373ac31c03bb96919e21726294015ad3b07f4
Jose Varela <varelacen@hotmail.com>
2021-01-31 16:08:10 -0300

    Para que no quede otra caga

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer sprite;
+    public Color color1;
+    public Color color2;
+    public Color color3;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
